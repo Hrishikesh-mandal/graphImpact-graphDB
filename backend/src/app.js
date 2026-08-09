df5eadf8106e7499cd35260e1bdc3e4dc3d58dee
Hrishikesh Mandal <hrishikesh003m@gmail.com>
2026-08-09 15:32:56 +0530

    Add CORS support

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const graphRoutes = require("./routes/graphRoutes");
 const errorHandler = require("./middleware/errorHandler");
-
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 // Parse incoming JSON requests
 app.use(express.json());
 
